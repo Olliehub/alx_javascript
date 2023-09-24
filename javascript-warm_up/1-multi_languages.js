cbefac4e9078c012eafdf05fdfcbe6230b265298
Olliehub <charlibossubom@gmail.com>
2023-09-24 21:19:08 +0100

    Changes made to languages project

--- javascript-warm_up/1-multi_languages.js
+++ javascript-warm_up/1-multi_languages.js
@@ -8,8 +8,4 @@ const thirdLine = "JavaScript is amazing";
 Write a script that prints 3 lines:
 */
 
-console.log(
-    'C is fun\
-Python is cool\
-Javascript is amazing'
-);
+console.log('C is fun\nPython is cool\nJavascript is amazing');
