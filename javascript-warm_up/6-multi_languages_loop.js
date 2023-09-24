abd81722c082f5e7700425833b805d9213b8fa2a
Olliehub <charlibossubom@gmail.com>
2023-09-24 21:53:03 +0100

    Printing from loop to languages

---
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+/*
+Write a script that prints 3 lines: (like 1-multi_languages.js)
+but by using an array of string and a loop
+*/
+
+const c_ = 'C is fun';
+const python_ = 'Python is cool';
+const java_ = 'JavaScript is amazing';
+const progLanguages = [c_, python_, java_];
+
+for (let i = 0; i < 3; i++) {
+  console.log(progLanguages[i]);
+}
--- javascript-warm_up/README.md
+++ javascript-warm_up/README.md
@@ -3,3 +3,4 @@ JavaScript - Warm up
 Task 0: First constant, First print. In this project, write a script that prints "Javascript is amazing"
 
 task 1: Printing still
+Task 2: Still on printing scripts
