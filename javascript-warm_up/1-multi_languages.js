4f9f3e7f49a980fc24b9a448bbcf80af5c4ba4cb
Olliehub <charlibossubom@gmail.com>
2023-09-24 14:59:05 +0100

    changes on languages

--- javascript-warm_up/1-multi_languages.js
+++ javascript-warm_up/1-multi_languages.js
@@ -1,7 +1,12 @@
 #!/usr/bin/node
 
+const firstLine = "C is fun";
+const secondLine = "Python is cool";
+const thirdLine = "JavaScript is amazing";
+
 /*
 Write a script that prints 3 lines:
 */
-
-console.log('C is fun\nPython is cool\nJavaScript is amazing');
+console.log(firstLine);
+console.log(secondLine);
+console.log(thirdLine);
