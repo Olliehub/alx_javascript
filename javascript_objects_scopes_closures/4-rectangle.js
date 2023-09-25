28a28004ce9cacdf3c5476f1a6118dcb7a01e7a4
Olliehub <charlibossubom@gmail.com>
2023-09-25 18:32:19 +0100

    Still on rectangle that defines another rectangle

---
+++ javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+
+class Rectangle {
+
+    // initialize instance attributes with these values //
+
+    constructor (w, h) {
+        if (w && h > 0) {
+            this.width = w;
+            this.height = h;
+        }else {
+            return 'Rectangle {}';
+        }
+    }
+
+    print () {
+        for (let i = 1; i <= this.height; i++) {
+            const rect_X = 'X'.repeat(this.width);
+            console.log(rect_X);
+        }
+    }
+}
+
+module.exports = Rectangle;
