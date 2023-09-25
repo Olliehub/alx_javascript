8c20be4a9500f95e5b14d3e4546b6553944fbeca
Olliehub <charlibossubom@gmail.com>
2023-09-25 14:53:40 +0100

    Rectangle #3

---
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+
+class Rectangle {
+
+    // initialize an instance attribute with these values //
+    
+    constructor (w, h) {
+        if (w && h > 0) {
+            this.width = w;
+            this.height = h;
+        }else {
+            return 'Rectangle {}';
+        }
+    }
+
+    print () {
+        for (let i = 1; i <= this.height; i++) {
+            const rect_X = 'X'.repeat(this.width);
+            console.log(rect_X);
+        }
+    }
+}
+
+module.exports = Rectangle;
