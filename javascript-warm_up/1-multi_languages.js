57659284de3f51c58f289d02fd033aaf04ca6ec3
Olliehub <charlibossubom@gmail.com>
2023-09-25 07:26:41 +0100

    changes on multi language

--- javascript-warm_up/1-multi_languages.js
+++ javascript-warm_up/1-multi_languages.js
@@ -2,7 +2,7 @@
 
 const firstLine = "C is fun";
 const secondLine = "Python is cool";
-const thirdLine = "JavaScript is amazing";
+const thirdLine = "Javascript is amazing";
 
 /*
 Write a script that prints 3 lines:
