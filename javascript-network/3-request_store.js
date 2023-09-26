00958ce493a9c01353ebadbbf9f55f66aafca97e
Olliehub <charlibossubom@gmail.com>
2023-09-26 18:59:04 +0100

    Loripsum

---
+++ javascript-network/3-request_store.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+/* A script that gets the contents of a webpage and stores it in a file. */
+
+//import modules //
+const request = require ('request');
+const fs = require ('fs');
+
+//argumets on command line //
+url = process.argv[2];
+file_name = process.argv[3];
+
+//make request //
+request.get(url).pipe(fs.createWriteStream(file_name));
