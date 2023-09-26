1ebad4e0fa2b40fd19881e720faab422aa0f808a
Olliehub <charlibossubom@gmail.com>
2023-09-26 22:01:25 +0100

    completed tasks

--- javascript-network/4-completed_tasks.js
+++ javascript-network/4-completed_tasks.js
@@ -2,51 +2,41 @@
 
 const request = require('request');
 
-const apiUrl = process.argv[2];
+// Define the API URL for all tasks //
+const apiUrl = 'https://jsonplaceholder.typicode.com/todos';
 
-request.get(apiUrl, (error, response, body) => {
+// Make a GET request to the API to fetch all tasks //
+request.get(apiUrl, { encoding: 'utf-8' }, (error, response, body) => {
   if (error) {
-    console.error('Error:', error);
-    return;
-  }
+    console.error(`Error: ${error.message}`);
+  } else if (response.statusCode !== 200) {
+    console.error(`Request failed with status code: ${response.statusCode}`);
+  } else {
+    try {
+      const tasks = JSON.parse(body);
 
-  if (response.statusCode !== 200) {
-    console.error('Request failed with status code:', response.statusCode);
-    return;
-  }
+      // Create an object to store the count of completed tasks for each user ID //
+      const userCompletedTasks = {};
 
-  try {
-    // Parse the JSON response //
-    const todos = JSON.parse(body);
+      // Count completed tasks for each user //
+      tasks.forEach((task) => {
+        if (task.completed) {
+          const userId = task.userId;
+          if (userCompletedTasks[userId]) {
+            userCompletedTasks[userId]++;
+          } else {
+            userCompletedTasks[userId] = 1;
+          }
+        }
+      });
 
-    // Filter completed tasks //
-    const completedTasks = todos.filter((todo) => todo.completed);
-
-    // Create an object to store the count of completed tasks for each user ID //
-    const userCompletedTasks = {};
-
-    // Count completed tasks for each user //
-    completedTasks.forEach((task) => {
-      const userId = task.userId;
-      if (userCompletedTasks[userId]) {
-        userCompletedTasks[userId]++;
-      } else {
-        userCompletedTasks[userId] = 1;
+      // Print the results //
+      for (const userId in userCompletedTasks) {
+        console.log(`User ID ${userId}: ${userCompletedTasks[userId]} completed tasks`);
       }
-    });
-
-    // Print the results //
-    for (const userId in userCompletedTasks) {
-    
-      console.log(`{'${userId}': ${userCompletedTasks[userId]}}`);
+    } catch (e) {
+      console.error(`Error parsing JSON: ${e.message}`);
     }
-  } catch (e) {
-    console.error('Error parsing JSON:', e);
-  }
-
-  // Print the results //
-for (const userId in userCompletedTasks) {
-    console.log(`{"${userId}": ${userCompletedTasks[userId]}}`);
   }
   
 });
